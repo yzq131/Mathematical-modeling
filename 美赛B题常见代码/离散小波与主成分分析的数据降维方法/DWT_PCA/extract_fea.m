6d75ac187c238e57a81dea46d7e89f56f5c99ff2
yzq131 <85005570+yzq131@users.noreply.github.com>
2023-09-20 21:11:25 +0800

    Delete 美赛B题常见代码 directory

--- 美赛B题常见代码/离散小波与主成分分析的数据降维方法/DWT_PCA/extract_fea.m
+++
@@ -1,7 +0,0 @@
-% extract the DCT-based features
-
-function dwt_im=extract_fea(im)
-
-[cA,cH,cV,cD] = dwt2(im,'db1'); 
-dwt_im= cA;
-
--- 美赛B题常见代码/离散粒子群算法DPSO优化代码/and_M.m
+++
@@ -1,3 +0,0 @@
-function result_M=and_M(M1,M2)
-%% 交集计算函数
-result_M=M1 | M2;
