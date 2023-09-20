09d542b1913fb5a4dc4666e4ae8ac7459bbafa9f
yzq131 <85005570+yzq131@users.noreply.github.com>
2023-09-20 21:11:18 +0800

    Delete 美赛A题常见代码 directory

--- 美赛A题常见代码/微分方程模型/program/program/dfun3.m
+++
@@ -1,4 +0,0 @@
-function bd=dfun3(t,b);
-bd=zeros(2,1);
-bd(1)=b(2);
-bd(2)=-b(1)-b(2)+b(1)^3;
--- 美赛A题常见代码/普通多目标优化算法代码/demo.m
+++
@@ -1,9 +0,0 @@
-function demo()
-
-mop = testmop('zdt1',30);
-
-pareto = moead( mop, 'popsize', 100, 'niche', 20, 'iteration', 200, 'method', 'te');
-
-%pareto = moead( mop, 'popsize', 100, 'niche', 20, 'iteration', 200, 'method', 'ws');
-
-end
